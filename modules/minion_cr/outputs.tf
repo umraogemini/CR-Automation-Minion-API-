32d705475051cab587c697d5dcdbc6ef32a785e0
umraogemini <umraogemini@gmail.com>
2025-03-25 11:58:26 +0530

    Create outputs.tf

---
+++ modules/minion_cr/outputs.tf
@@ -0,0 +1,5 @@
+# Module Outputs - Outputs CR ID for tracking
+output "cr_id" {
+  description = "The ID of the created Change Request"
+  value       = http_request.create_cr.response_body.cr_id
+}
